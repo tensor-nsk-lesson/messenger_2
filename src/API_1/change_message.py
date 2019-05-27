1c81fe6e0f71a96972a1ad667105a54f08d65035
Shamaus <Shamaus@bk.ru>
2019-05-27 16:15:16 +0700

    change_message

---
+++ src/API_1/change_message.py
@@ -0,0 +1,26 @@
+from flask import Flask, request
+import psycopg2
+
+app = Flask(__name__)
+# Изменение сообщение
+@app.route("/chat", methonds=['POST'])
+def login():
+    connect = psycopg2.connect("host=90.189.168.29 dbname=messenger_2 user=messenger_2 password=messenger_2")
+    cursor = connect.cursor()
+    cursor.execute("SET search_path TO public")
+    id_mess = request.form['id_mess']
+    chat_id = request.form['chat_id']
+    user_id = request.form['user_id']
+    cursor.execute("UPDATE \"Message\" SET 'del_mes' = '1' WHERE 'id_message' = " + id_mess + ";")
+    cursor.execute("SELECT count(*) FROM \"Message\"")
+    for table in cursor.fetchone():
+        num = table[0] + 1
+        num = str(num)
+    cursor.execute("INSERT INTO \"Message\" (id_message, id_chat, id_user, time, id_status, del_mes)" 
+                   "VALUES(" + num + ", " + chat_id + ", " + user_id + ", now(),'0', '0');")
+    cursor.close()
+    connect.commit()
+    connect.close()
+
+if __name__ == "__main__":
+    app.run()
