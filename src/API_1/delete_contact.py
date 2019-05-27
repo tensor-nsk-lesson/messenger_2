beb7ee485d4f71574f39d43ab4bf3d65c9982a6f
Shamaus <Shamaus@bk.ru>
2019-05-27 16:08:29 +0700

    delete_contact

--- src/API_1/delete_contact.py
+++ src/API_1/delete_contact.py
@@ -8,7 +8,7 @@ def login():
     connect = psycopg2.connect("host=90.189.168.29 dbname=messenger_2 user=messenger_2 password=messenger_2")
     cursor = connect.cursor()
     cursor.execute("SET search_path TO public")
-    id_cont = request.form['user_id_cont']
+    id_cont = request.form['id_cont']
     cursor.execute("UPDATE \"Contact\" SET 'cont_status' = '0' WHERE 'id_cont' = " + id_cont + ";")
     cursor.close()
     connect.commit()
