f721da9490d71d5b9090516e071dc8ba0465ad98
Shamaus <Shamaus@bk.ru>
2019-05-27 14:37:24 +0700

    Authorization

---
+++ src/API_1/login_api.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+import psycopg2
+
+app = Flask(__name__)
+# Авторизация
+@app.route("/login", methonds=['POST'])
+def login():
+    connect = psycopg2.connect("host=90.189.168.29 dbname=messenger_2 user=messenger_2 password=messenger_2")
+    cursor = connect.cursor()
+    cursor.execute("SET search_path TO public")
+    number = request.form['number']
+    password = request.form['password']
+    cursor.execute("SELECT * FOR \"Authorization\" WHERE 'user_number' = " + number + ";")
+    mes = ''
+    for table in cursor.fetchone():
+        if table[2] == number and table[3] == password:
+            return 'Succesfuly'
+    cursor.close()
+    connect.commit()
+    connect.close()
+    return 'lorin or password is not found'
+
+if __name__ == "__main__":
+    app.run()
