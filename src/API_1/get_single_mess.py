a3af0bcc221d191f6e887e05f65309858f78c36c
Shamaus <Shamaus@bk.ru>
2019-05-27 14:49:39 +0700

    get_single_mess

---
+++ src/API_1/get_single_mess.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+import psycopg2
+
+app = Flask(__name__)
+# Авторизация
+@app.route("/profile", methonds=['GET'])
+def login():
+    connect = psycopg2.connect("host=90.189.168.29 dbname=messenger_2 user=messenger_2 password=messenger_2")
+    cursor = connect.cursor()
+    cursor.execute("SET search_path TO public")
+    mes_id = request.form['mes_id']
+    cursor.execute("SELECT * FOR \"Message\" WHERE 'id_message' = " + mes_id + ";")
+    mes = ''
+    for table in cursor.fetchone():
+        mes = table.__str__() + "<br>"
+    cursor.close()
+    connect.commit()
+    connect.close()
+    return mes
+
+if __name__ == "__main__":
+    app.run()
